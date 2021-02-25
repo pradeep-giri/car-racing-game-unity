3c872208e642237781539d508ff30d549d15757b
Pradeep Giri <pradeepgiri.pg123@gmail.com>
2021-02-25 08:05:47 +0530

    Main Canvas UI Added

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameView");
+        SceneManager.UnloadSceneAsync("MainPageUI");
+    }
+}
